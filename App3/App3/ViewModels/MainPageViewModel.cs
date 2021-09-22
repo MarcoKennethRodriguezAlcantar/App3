2c65810614e6f06e801715f2fa4178f4be90342d
Marco <Marco@DESKTOP-D4T6Q5N>
2021-09-22 16:45:02 -0500

    Agregar archivos de proyecto.

---
+++ App3/App3/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App3.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private string _nombre;
+        private string _paterno;
+        private string _materno;
+
+        public MainPageViewModel()
+        {
+            _nombre = "";
+            _paterno = "";
+            _materno = "";
+
+        }
+
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                _nombre = value;
+                RaisePropertyChanged(nameof(Nombre));
+                RaisePropertyChanged(nameof(NombreCompleto));
+            }
+        }
+
+        public string Paterno
+        {
+            get => _paterno;
+            set
+            {
+                _paterno = value;
+                RaisePropertyChanged(nameof(Paterno));
+                RaisePropertyChanged(nameof(NombreCompleto));
+            }
+        }
+
+        public string Materno
+        {
+            get => _materno;
+            set
+            {
+                _materno = value;
+                RaisePropertyChanged(nameof(Materno));
+                RaisePropertyChanged(nameof(NombreCompleto));
+            }
+        }
+
+        public string NombreCompleto
+        {
+
+            get => string.Format("{0} {1} {2}", _nombre, _paterno, _materno);
+
+        }
+
+    }
+}
